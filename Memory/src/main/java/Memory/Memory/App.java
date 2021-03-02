354fe1e6b216fef9f8891a1f13507d4275889ab9
MathiasDebrun <mathiasdebrun@hotmail.com>
2021-03-02 15:43:19 +0100

    Primer Commit

---
+++ Memory/src/main/java/Memory/Memory/App.java
@@ -0,0 +1,13 @@
+package Memory.Memory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
