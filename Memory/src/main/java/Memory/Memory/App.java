efee686d93c9ef200664e61e9967af38bd453704
Estefania Colombo Rosario <estefania.c.r@hotmail.com>
2021-03-05 15:44:36 +0100

    Memory Final con comentarios

--- Memory/src/main/java/Memory/Memory/App.java
+++ Memory/src/main/java/Memory/Memory/App.java
@@ -1,13 +1,23 @@
 package Memory.Memory;
 
-/**
- * Hello world!
- *
- */
+import java.awt.EventQueue;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana_Memory frame = new Ventana_Memory();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
